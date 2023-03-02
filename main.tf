65b90c2f932af9c68427a2acbf47c6b31c4f627b
YitzhakyBar <123241700+YitzhakyBar@users.noreply.github.com>
2023-03-02 16:39:03 +0200

    5

--- main.tf
+++ main.tf
@@ -10,10 +10,8 @@ terraform {
   required_version = ">= 1.2.0"
 }
 
-
-# Define AWS as provider
 provider "aws" {
-  region = "us_east_2"
+  region = "us-east-2"
 }
 
 
@@ -26,6 +24,216 @@ resource "aws_vpc" "status_page_vpc" {
     Description = "This is a VPC for status_page infrastructure"
   }
 }
+
+
+# Create public subnet 1
+resource "aws_subnet" "status_page_public_subnet1" {
+  vpc_id            = aws_vpc.status_page_vpc.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "us-east-2c"
+
+  tags = {
+    Name        = "status_page_public_subnet1"
+    Description = "This is a public subnet 1 for bastion 1"
+  }
+}
+
+# Associate public subnet 1 with the route table
+resource "aws_route_table_association" "public_subnet1_assoc" {
+  subnet_id      = aws_subnet.status_page_public_subnet1.id
+  route_table_id = aws_route_table.status_page_route_table_igw.id
+}
+
+
+# Create public subnet 2
+resource "aws_subnet" "status_page_public_subnet2" {
+  vpc_id            = aws_vpc.status_page_vpc.id
+  cidr_block        = "10.0.2.0/24"
+  availability_zone = "us-east-2b"
+
+  tags = {
+    Name        = "status_page_public_subnet2"
+    Description = "This is a public subnet 2 for bastion 2"
+  }
+}
+
+# Associate public subnet 2 with the route table
+resource "aws_route_table_association" "public_subnet2_assoc" {
+  subnet_id      = aws_subnet.status_page_public_subnet2.id
+  route_table_id = aws_route_table.status_page_route_table_igw.id
+}
+
+
+# Create private subnet 1
+resource "aws_subnet" "status_page_private_subnet1" {
+  vpc_id            = aws_vpc.status_page_vpc.id
+  cidr_block        = "10.0.3.0/24"
+  availability_zone = "us-east-2a"
+
+  tags = {
+    Name = "status_page_private_subnet1"
+  }
+}
+
+# Associate the private route table with the private subnet
+resource "aws_route_table_association" "status_page_private_association" {
+  route_table_id = aws_route_table.private_subnet_route_table.id
+  subnet_id      = aws_subnet.status_page_private_subnet1.id
+}
+
+
+# Create private subnet 2
+resource "aws_subnet" "status_page_private_subnet2" {
+  vpc_id            = aws_vpc.status_page_vpc.id
+  cidr_block        = "10.0.4.0/24"
+  availability_zone = "us-east-2b"
+
+  tags = {
+    Name = "status_page_private_subnet2"
+  }
+}
+
+# Associate the private route table with the private subnet
+resource "aws_route_table_association" "status_page_private_association" {
+  route_table_id = aws_route_table.private_subnet_route_table.id
+  subnet_id      = aws_subnet.status_page_private_subnet2.id
+}
+
+
+# Create a route table for the private subnet
+resource "aws_route_table" "private_subnet_route_table" {
+  vpc_id = aws_vpc.status_page_vpc.id
+
+  route {
+    cidr_block     = "10.0.1.0/24"
+    nat_gateway_id = aws_nat_gateway.status_page_nat_gateway.id
+  }
+
+  tags = {
+    Name = "Private Route Table for nat in status page"
+  }
+}
+
+
+
+
+
+# create bastion1 security group
+resource "aws_security_group" "bastion1_security_group" {
+  name        = "bastion1_security_group"
+  description = "Allow SSH access to bastion1 from my local pc"
+  vpc_id      = aws_vpc.status_page_vpc.id
+
+
+  ingress {
+    from_port = 22
+    to_port   = 22
+    protocol  = "tcp"
+    cidr_blocks = ["77.125.2.122/32", "77.137.65.124/32"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name        = "bastion1_security_group"
+    Description = "security group for 1st bastion"
+  }
+}
+
+
+# create bastion2 security group
+resource "aws_security_group" "bastion2_security_group" {
+  name        = "bastion2_security_group"
+  description = "Allow SSH access to bastion2 from my local pc"
+  vpc_id      = aws_vpc.status_page_vpc.id
+
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["77.125.2.122/32", "77.137.65.124/32"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name        = "bastion2_security_group"
+    Description = "security group for 2nd bastion"
+  }
+}
+
+
+# create production1 security group
+resource "aws_security_group" "production1_security_group" {
+  name        = "production1_security_group"
+  description = "Allow SSH access to production1 from bastion security groups"
+  vpc_id      = aws_vpc.status_page_vpc.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["10.0.1.0/24", "10.0.2.0/24"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name        = "production1_security_group"
+    Description = "Allow SSH access to production1 from bastion security groups"
+  }
+}
+
+
+# create production2 security group
+resource "aws_security_group" "production2_security_group" {
+  name        = "production1_security_group"
+  description = "Allow SSH access to production1 from bastion security groups"
+  vpc_id      = aws_vpc.status_page_vpc.id
+
+  ingress {
+    from_port   = 8000
+    to_port     = 8000
+    protocol    = "tcp"
+    cidr_blocks = ["10.0.1.0/24", "10.0.2.0/24"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name        = "production2_security_group"
+    Description = "Allow SSH access to production1 from bastion security groups"
+  }
+}  */
+
+
+
+
+
+
+
+
+
 
 # Create internet gateway for public subnet
 resource "aws_internet_gateway" "status_page_igw" {
@@ -74,200 +282,156 @@ resource "aws_nat_gateway" "status_page_nat_gateway" {
   }
 }
 
-# Create a route table for the private subnet
-resource "aws_route_table" "private_subnet_route_table" {
-  vpc_id = aws_vpc.status_page_vpc.id
 
-  route {
-    cidr_block     = "10.0.1.0/24"
-    nat_gateway_id = aws_nat_gateway.status_page_nat_gateway.id
-  }
-
-  tags = {
-    Name = "Private Route Table for nat in status page"
-  }
-}
-
-
-# Associate the private route table with the private subnet
-resource "aws_route_table_association" "status_page_private_association" {
-  route_table_id = aws_route_table.private_subnet_route_table.id
-  subnet_id      = aws_subnet.status_page_private_subnet1.id
-}
-
-
-# Create public subnet 1
-resource "aws_subnet" "status_page_public_subnet1" {
-  vpc_id            = aws_vpc.status_page_vpc.id
-  cidr_block        = "10.0.1.0/24"
-  availability_zone = "us-east-2a"
+# create a bastion1
+resource "aws_instance" "status_page_bastion1" {
+  ami                    = "ami-0f3c9c466bb525749"
+  instance_type          = "t2.micro"
+  key_name               = "key_bar_ron"
+  subnet_id              = aws_subnet.status_page_public_subnet1.id
+  vpc_security_group_ids = [aws_security_group.bastion1_security_group.id]
 
   tags = {
-    Name        = "status_page_public_subnet1"
-    Description = "This is a public subnet 1 for bastion 1"
+    Name = "status_page_bastion1"
   }
 }
 
 
-# Create public subnet 2
-resource "aws_subnet" "status_page_public_subnet2" {
-  vpc_id            = aws_vpc.status_page_vpc.id
-  cidr_block        = "10.0.2.0/24"
-  availability_zone = "us-east-2b"
+# create a bastion2
+resource "aws_instance" "status_page_bastion2" {
+  ami                    = "ami-0f3c9c466bb525749"
+  instance_type          = "t2.micro"
+  key_name               = "key_bar_ron"
+  subnet_id              = aws_subnet.status_page_public_subnet2.id
+  vpc_security_group_ids = [aws_security_group.bastion2_security_group.id]
 
   tags = {
-    Name        = "status_page_public_subnet2"
-    Description = "This is a public subnet 2 for bastion 2"
+    Name = "status_page_bastion2"
   }
-}
+} 
 
 
-# Associate public subnet 1 with the route table
-resource "aws_route_table_association" "public_subnet1_assoc" {
-  subnet_id      = aws_subnet.status_page_public_subnet1.id
-  route_table_id = aws_route_table.status_page_route_table_igw.id
-}
 
-
-# Associate public subnet 2 with the route table
-resource "aws_route_table_association" "public_subnet2_assoc" {
-  subnet_id      = aws_subnet.status_page_public_subnet2.id
-  route_table_id = aws_route_table.status_page_route_table_igw.id
-}
-
-
-# Create private subnet 1
-resource "aws_subnet" "status_page_private_subnet1" {
-  vpc_id            = aws_vpc.status_page_vpc.id
-  cidr_block        = "10.0.1.0/24"
-  availability_zone = "us-east-2a"
-}
-
-
-# Create private subnet 2
-resource "aws_subnet" "status_page_private_subnet2" {
-  vpc_id            = aws_vpc.status_page_vpc.id
-  cidr_block        = "10.0.2.0/24"
-  availability_zone = "us-east-2b"
-}
-
-
-resource "aws_security_group" "bastion1_security_group" {
-  name        = "bastion1_security_group"
-  description = "Allow SSH access to bastion1 from my local pc"
-  vpc_id      = aws_vpc.status_page_vpc.id
-
-
+/* 
+# create a sg to elb 
+resource "aws_security_group" "alb_sg" {
+  name_prefix = "alb_sg"
   ingress {
-    from_port   = 22
-    to_port     = 22
+    from_port   = 80
+    to_port     = 80
     protocol    = "tcp"
-    cidr_blocks = ["77.125.2.122/32", "77.137.65.124/32"]
-  }
-   egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
-  }  
-
-  tags = {
-    Name        = "bastion1_security_group"
-    Description = "security group for 1st bastion"
   }
 }
 
-
-resource "aws_security_group" "bastion2_security_group" {
-  name        = "bastion2_security_group"
-  description = "Allow SSH access to bastion2 from my local pc"
-  vpc_id      = aws_vpc.status_page_vpc.id
-
-
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["77.125.2.122/32", "77.137.65.124/32"]
-  }
-
-   egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+# create ELB for the prodaction
+resource "aws_lb" "status_page_elb" {
+  name               = "status_page_elb"
+  internal           = true
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.alb_sg.id]
+  subnets            = [aws_subnet.status_page_private_subnet1.id, aws_subnet.status_page_private_subnet2.id]
 
   tags = {
-    Name        = "bastion2_security_group"
-    Description = "security group for 2nd bastion"
+    Name = "status_page_elb"
   }
 }
 
+resource "aws_lb_listener" "stastus_page_lb_listener" {
+  load_balancer_arn = aws_lb.example.arn
+  port              = "80"
+  protocol          = "HTTP"
+#  ssl_policy        = "ELBSecurityPolicy-2016-08" -----
+#  certificate_arn   = "arn:aws:acm:us-west-2:123456789012:certificate/abcd1234-5678-90ef-ghij-klmn1234abcd" ------
 
-resource "aws_security_group" "production1_security_group" {
-  name        = "production1_security_group"
-  description = "Allow SSH access to production1 from bastion security groups"
-  vpc_id      = aws_vpc.status_page_vpc.id
-
-  ingress {
-    from_port                = 8000
-    to_port                  = 8000
-    protocol                 = "tcp"
-    security_groups = ["${aws_security_group.bastion1_sg.id}"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  } 
-
-  tags = {
-    Name        = "production1_security_group"
-    Description = "Allow SSH access to production1 from bastion security groups"
+  default_action {
+    target_group_arn = aws_lb_target_group.status_page_lb_target_group.arn
+    type             = "forward"
   }
 }
 
-resource "aws_security_group" "production2_security_group" {
-  name        = "production1_security_group"
-  description = "Allow SSH access to production1 from bastion security groups"
-  vpc_id      = aws_vpc.status_page_vpc.id
+resource "aws_lb_target_group" "status_page_lb_target_group" {
+  name     = "status_page_lb_target_group"
+  port              = "80"
+  protocol          = "HTTP"
+  vpc_id   = aws_vpc.status_page_vpc.id
 
-  ingress {
-    from_port                = 8000
-    to_port                  = 8000
-    protocol                 = "tcp"
-    cidr_blocks = ["77.125.2.122/32", "77.137.65.124/32"]
-    security_groups = ["${aws_security_group.bastion2_sg.id}"]
+  health_check {
+    enabled = true
+    interval = 30
+    timeout = 5
+    protocol = "HTTPS"
+    port     = 80
+    path = "/"
+    matcher = "200-399"
+    tls      = true 
   }
 
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = {
-    Name        = "production2_security_group"
-    Description = "Allow SSH access to production1 from bastion security groups"
+  stickiness {
+    type         = "lb_cookie"
+    cookie_duration = 600
+    cookie_name      = "sticky-cookie"
+    cookie_secure    = true
+    cookie_httponly  = true
   }
 }
 
+# create a ECS cluster that deploys two EC2 instances  
+resource "aws_ecs_cluster" "status_page_ecs" {
+  name = "status_page_ecs"
+}
+
+resource "aws_ecs_task_definition" "status_page_task" {
+  family                   = "cluster-task"
+  container_definitions    = <<DEFINITION
+[
+  {
+    "name": "web",
+    "image": "nginx:latest", --- docker image ----
+    "portMappings": [
+      {
+        "containerPort": 8000,
+        "hostPort": 8000
+      }
+    ]
+    "environment": [
+      {
+        "name": "APP_ENV",
+        "value": "prod"
+      }
+    ]
+  }
+]
+DEFINITION
+}
+
+resource "aws_ecs_service" "status_page_ec2" {
+  name            = "ec2-service"
+  cluster         = aws_ecs_cluster.status_page_ecs.id
+  task_definition = aws_ecs_task_definition.status_page_task.arn
+  desired_count   = 2
+  launch_type     = "EC2"
+
+  network_configuration {
+    subnet_ids      = [aws_subnet.status_page_private_subnet1.id, aws_subnet.status_page_private_subnet2.id]
+    security_groups = [aws_security_group.production1_security_group.id, aws_security_group.production2_security_group.id]
+  }
+
+  load_balancer {
+    target_group_arn = aws_lb_target_group.status_page_lb_target_group.arn
+    container_name   = "web"
+    container_port   = 8000
+  }
+}  
+
+# create a ci serever cluster with ecs 
 
 
 
+*/
 
 
- 
-
-
-
-
-
-# create a WAF rules
+/* # create a WAF rules
 variable "waf_rules" {
   type    = list(string)
   default = [
@@ -350,106 +514,8 @@ variable "waf_rules" {
 
 
 
-# create ELB for the prodaction
-resource "aws_lb" "status_page_elb" {
-  name               = "status_page_elb"
-  internal           = true
-  load_balancer_type = "application"
-  subnets            = [aws_subnet.status_page_private_subnet1.id, aws_subnet.status_page_private_subnet2.id]
 
-  security_groups = [
-    aws_security_group.production1_security_group.id, aws_security_group.production2_security_group.id,
-  ]
 
-  tags = {
-    Name = "status_page-lb"
-  }
-}
 
-resource "aws_lb_listener" "stastus_page_lb_listener" {
-  load_balancer_arn = aws_lb.example.arn
-  port              = "443"
-  protocol          = "HTTPS"
-  ssl_policy        = "ELBSecurityPolicy-2016-08" -----
-  certificate_arn   = "arn:aws:acm:us-west-2:123456789012:certificate/abcd1234-5678-90ef-ghij-klmn1234abcd" ------
 
-  default_action {
-    target_group_arn = aws_lb_target_group.status_page_lb_target_group.arn
-    type             = "forward"
-  }
-}
-
-resource "aws_lb_target_group" "status_page_lb_target_group" {
-  name     = "status_page_lb_target_group"
-  port              = "443"
-  protocol          = "HTTPS"
-  vpc_id   = aws_vpc.status_page_vpc.id
-
-  health_check {
-    enabled = true
-    interval = 30
-    timeout = 5
-    protocol = "HTTPS"
-    port     = 443
-    path = "/"
-    matcher = "200-399"
-    tls      = true 
-  }
-
-  stickiness {
-    type         = "lb_cookie"
-    cookie_duration = 600
-    cookie_name      = "sticky-cookie"
-    cookie_secure    = true
-    cookie_httponly  = true
-  }
-}
-
-# create a ECS cluster that deploys two EC2 instances  
-resource "aws_ecs_cluster" "status_page_ecs" {
-  name = "status_page_ecs"
-}
-
-resource "aws_ecs_task_definition" "status_page_task" {
-  family                   = "cluster-task"
-  container_definitions    = <<DEFINITION
-[
-  {
-    "name": "web",
-    "image": "nginx:latest", --- docker image ----
-    "portMappings": [
-      {
-        "containerPort": 8000,
-        "hostPort": 8000
-      }
-    ]
-    "environment": [
-      {
-        "name": "APP_ENV",
-        "value": "prod"
-      }
-    ]
-  }
-]
-DEFINITION
-}
-
-resource "aws_ecs_service" "status_page_ec2" {
-  name            = "ec2-service"
-  cluster         = aws_ecs_cluster.status_page_ecs.id
-  task_definition = aws_ecs_task_definition.status_page_task.arn
-  desired_count   = 2
-  launch_type     = "EC2"
-
-  network_configuration {
-    subnet_ids      = [aws_subnet.status_page_private_subnet1.id, aws_subnet.status_page_private_subnet2.id]
-    security_groups = [aws_security_group.production1_security_group.id, aws_security_group.production2_security_group.id]
-  }
-
-  load_balancer {
-    target_group_arn = aws_lb_target_group.status_page_lb_target_group.arn
-    container_name   = "web"
-    container_port   = 8000
-  }
-}
- 
+*/
